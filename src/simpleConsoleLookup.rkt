18c6e8c1f7b1b3cbba3381df1d902cf539da9c8b
Weiqifan1 <cmlykke@hotmail.com>
2020-11-28 18:01:10 +0100

    finished simpel console app. need to be improved so that the user doesnt have to select a character manually

--- src/simpleConsoleLookup.rkt
+++ src/simpleConsoleLookup.rkt
@@ -3,25 +3,7 @@
 (require "deserializeStaticFiles.rkt")
 
 (hash-ref cangjie5Code "bmi")
-
-(define (handleInput str_userInput)
-  ;(identity str_userInput))
-  (define inputSystemResult (if (hash-has-key? cangjie5Code (string-trim str_userInput))
-      (hash-ref cangjie5Code (string-trim str_userInput))
-      (identity "")))
-  (if (list? inputSystemResult)
-      (first inputSystemResult)
-      (identity inputSystemResult)))
   
-  
-
-  
-;should return a string
-(define (displayListAndInputContent li_previousResults str_input)
-  (apply string-append
-  (append li_previousResults
-  (list
-  (handleInput str_input)))))
 
 ;returns a list of inputsystem hits. It is empty if there are none
 (define (getCandidates str_userInput)
@@ -29,6 +11,24 @@
       (hash-ref cangjie5Code (string-trim str_userInput))
       (identity '())))
   (identity inputSystemResult))
+
+;get a list of possibleCandidates and ask the user to choose among them
+;if the list is empty, it retuns an empty string
+(define (chooseCandidate li_candidates)
+  (cond [(empty? li_candidates)
+         (display "no hits\n")
+         (identity "")]
+        [else
+         (display (string-append "choseOne: "
+                   (apply string-append (map
+                   (lambda (eachCandidate) (identity eachCandidate))
+                   li_candidates))))
+         (display "\n")
+         (define userChooseResult (read-line (current-input-port) 'any))
+         (define chosenInput (- (string->number userChooseResult) 1))
+         (vector-ref (list->vector li_candidates) chosenInput)
+         ]))
+ 
 
 ;console program tager en string fra brugeren og skriver en string som output.
 ;outputtet er resultatet af funktionen "processConsoleInput a" .
@@ -38,11 +38,13 @@
   (define userInput (read-line (current-input-port) 'any))
   (printf "\n")
       (cond [(equal? userInput "quit") "the program is over"]
-            [else           
-             (define previousResults (displayListAndInputContent lst userInput))
-             (display previousResults)
+            [else
+             (define candidates (getCandidates userInput))
+             (define pickACandidate (chooseCandidate candidates))             
+             (define resultOfPick (string-append (apply string-append lst) pickACandidate))
+             (display resultOfPick)           
              (display "\n")
-             (iter (list previousResults))]))
+             (iter (list resultOfPick))]))
 
 ;skriv det program jeg kalder
 (define run (iter '()))
