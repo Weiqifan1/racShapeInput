eb1ae2cbe1ace4213d31efca014078c711a3366f
Weiqifan1 <cmlykke@hotmail.com>
2020-12-11 17:34:54 +0100

    the random.rkt file is ready to be integrated with the inputsystems, but first I should add a real input field and use the editor panel as the selector field

--- src/random.rkt
+++ src/random.rkt
@@ -1,6 +1,12 @@
 #lang racket
 (require racket/gui)
 
+;use command field to to generate input for the main text field
+(define (getResultOfCommandCode StrCommandCode)
+  (identity "hejDer")) 
+
+;***************************** Gui Code **********************************
+
 ;first frame of the program
 (define firstFrame (new frame% [label "An editor that allow you to input commands"]
                       [width 500]
@@ -18,26 +24,23 @@
 (define (addStringToCmdField newString)
   (writeToCommandField (string-append (send commandField get-label) newString)))
 
-(define (handleCmdFieldInput keyEvent)
-  (if (equal? (send keyEvent get-key-code) (identity #\a))
-      (addStringToCmdField "A")
-      (addStringToCmdField (string (send keyEvent get-key-code)))))
+(define (handleCmdFieldInput keyEvent currentClass)
+  (if (equal? (send keyEvent get-key-code) (integer->char 32)) ;space character     
+      (begin (send currentClass insert (getResultOfCommandCode (send commandField get-label)))
+             (writeToCommandField "")
+             (send currentClass insert #"\backspace")
+             )
+      (begin (addStringToCmdField (string (send keyEvent get-key-code)))
+             (send currentClass insert #"\backspace"))))
 
 ;the text area
 (define mainCanvas (new editor-canvas% [parent firstFrame]))
-
-;(define mainTextArea (new (class text%
-;(define/override (on-char e)
-;  (when (send e get-caps-down)
-;(writeToCommandField e))
-;(super on-char e))              
-;(super-new))))
 
 (define mainTextArea (new (class text%
 (define/override (on-char e)
   (when (and (not (send e get-caps-down))
              (char? (send e get-key-code)))
-    (handleCmdFieldInput e))
+    (handleCmdFieldInput e this))
     ;(addStringToCmdField (string (send e get-key-code)))
     ;(send this insert #"\backspace")) ;this gives an error in console, but it works
 (super on-char e))              
